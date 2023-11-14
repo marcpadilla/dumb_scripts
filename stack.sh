c9fdb1faa2a9f5ba258911457fbd7c666130588f
Marc X. Padilla <marc@padil.la>
2023-11-13 19:23:28 -0600

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/zsh
 
 # Check if the first argument is "-h" or "--help" to display usage information.
-if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
     echo "Usage: $0 <pattern> <file> [-p/--progress]"
     echo "  <pattern>    : The search pattern."
     echo "  <file>       : The file to search in."
@@ -10,7 +10,7 @@ if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
 fi
 
 # Check if at least two arguments (pattern and file) are provided; otherwise, display usage instructions and exit.
-if [ $# -lt 2 ]; then
+if [[ $# -lt 2 ]]; then
     echo "Usage: $0 <pattern> <file> [-p/--progress]"
     exit 1
 fi
@@ -21,11 +21,11 @@ file="$2"
 total_lines=$(wc -l < "$file")
 
 # Check if the third argument is "--progress" or "-p" to enable progress tracking.
-if [ "$3" == "--progress" ] || [ "$3" == "-p" ]; then
-    # Use egrep to search for the pattern, pipe the output through 'pv' for progress tracking,
+if [[ "$3" == "--progress" || "$3" == "-p" ]]; then
+    # Use grep -E to search for the pattern, pipe the output through 'pv' for progress tracking,
     # and then process the results with sorting, counting, and formatting.
-    egrep -io "$pattern" "$file" | pv -l -s "$total_lines" -N "egrep progress" | sort | uniq -c | sort -nr | awk '{lines[NR]=$0; if (length($1) > max) max = length($1);} END { for (i=1; i<=NR; i++) { split(lines[i], fields); printf "%" max "d %s\n", fields[1], fields[2]; }}'
+    grep -E -io "$pattern" "$file" | pv -l -s "$total_lines" -N "grep progress" | sort | uniq -c | sort -nr | awk '{lines[NR]=$0; if (length($1) > max) max = length($1);} END { for (i=1; i<=NR; i++) { split(lines[i], fields); printf "%" max "d %s\n", fields[1], fields[2]; }}'
 else
-    # Use egrep to search for the pattern and process the results with sorting, counting, and formatting.
-    egrep -io "$pattern" "$file" | sort | uniq -c | sort -nr | awk '{lines[NR]=$0; if (length($1) > max) max = length($1);} END { for (i=1; i<=NR; i++) { split(lines[i], fields); printf "%" max "d %s\n", fields[1], fields[2]; }}'
+    # Use grep -E to search for the pattern and process the results with sorting, counting, and formatting.
+    grep -E -io "$pattern" "$file" | sort | uniq -c | sort -nr | awk '{lines[NR]=$0; if (length($1) > max) max = length($1);} END { for (i=1; i<=NR; i++) { split(lines[i], fields); printf "%" max "d %s\n", fields[1], fields[2]; }}'
 fi
